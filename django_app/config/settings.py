d52774335c38243ae18af46a08d443e774cb569a
cdragon-1 <streetfood89@gmail.com>
2017-03-14 07:16:50 +0900

    customUser 추가

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'member'
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
